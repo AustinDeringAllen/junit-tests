20ab03b07ebe84142c3d8a55f591653cce69c6a7
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-09-01 15:18:52 -0500

    Student class made

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Student {
+    private String name;
+    private long id;
+    private ArrayList<Integer> grades;
+
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+        this.grades = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        int total = 0;
+        for(int grade : grades) {
+            total += grade;
+        }
+        return (double) total / grades.size();
+    }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class StudentTest {
+
+    @Test
+    public void studentConstructorTest() {
+        Student student = new Student("Dylan",0);
+        assertNotNull(student.getName());
+        assertEquals(0,student.getId());
+        assertNotNull(student.getGrades());
+        assertTrue(student.getGrades().isEmpty());
+    }
+
+    @Test
+    public void studentAddGrade() {
+        Student student = new Student("Dylan",0);
+        assertTrue(student.getGrades().isEmpty());
+        student.addGrade(12);
+        assertEquals(1,student.getGrades().size());
+    }
+
+    @Test
+    public void studentChangeNameID() {
+        Student student = new Student("Dylan",0);
+        student.setName("Austin");
+        student.setId(12);
+        assertNotEquals("Dylan",student.getName());
+        assertNotEquals(0,student.getId());
+    }
+
+    @Test
+    public void studentGradeAverage() {
+        Student student = new Student("Dylan",0);
+        student.addGrade(100);
+        student.addGrade(82);
+        student.addGrade(95);
+        student.addGrade(99);
+        student.addGrade(85);
+        assertEquals(5,student.getGrades().size());
+        assertEquals(92.2,student.getGradeAverage(),0.1);
+
+    }
 }
